068a32f491d7423a7579bdc1578c6d635b93303f
haidarhalessa <haidarhalessa@gmail.com>
2026-01-01 14:33:36 +0300

    Add formula for Vapor Generator v1.0.0

---
+++ Formula/vgen.rb
@@ -0,0 +1,23 @@
+class Vgen < Formula
+  desc "A CLI tool to generate Vapor boilerplate components"
+  homepage "https://github.com/haidarhalessa/vapor-generator"
+  url "https://github.com/haidarhalessa/vapor-generator/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "eb2117978db071293b2fba5cbec2963faac030389b1af0759cdb01532835c7ee"
+  license "MIT"
+  
+  # Swift requires Xcode (or Swift toolchain) to build
+  depends_on xcode: ["13.0", :build]
+
+  def install
+    # 1. Build the project using Swift Package Manager
+    system "swift", "build", "--disable-sandbox", "-c", "release"
+    
+    # 2. Move the binary to the Homebrew bin location
+    bin.install ".build/release/vaporgen"
+  end
+
+  test do
+    # Verify it works
+    system "#{bin}/vaporgen", "--help"
+  end
+end
